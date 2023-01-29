06fcba1ca0450cf1f1d68efa1ae103b3f9a5ca14
HafizdXD <101934492+HAFIZDXD@users.noreply.github.com>
2023-01-29 08:55:54 +0700

    Update groupbot.js

--- plugins/groupbot.js
+++ plugins/groupbot.js
@@ -6,7 +6,7 @@ const sections = [
    {
 	title: `𝗝𝗢𝗜𝗡 𝗞𝗘 𝗚𝗥𝗨𝗣 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗕𝗢𝗧*\n\n\n━━━〔 Dᴏɴ'ᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕━━━`,
 	rows: [
-	    {title: '💌 › Group NexBotz', description: "Group Utama Fangz NexBotz", rowId:".gcbot"}
+	    {title: '💌 › Group MakimaBotz', description: "Group Utama", rowId:".gcbot"}
 	    ]
       },
 ]
