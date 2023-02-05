40e0109b34764de30cd35279840ee1fc0cd1ab0d
HafizdXD <101934492+HAFIZDXD@users.noreply.github.com>
2023-02-05 09:52:28 +0700

    Update openai.js

--- plugins/openai.js
+++ plugins/openai.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 let handler = async (m, { conn, text }) => {
 if (!text) throw "[!] Masukkan teks."
 const configuration = new Configuration({
-    apiKey: "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"
+    apiKey: "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"
 });
 const openai = new OpenAIApi(configuration);
         const response = await openai.createCompletion({
